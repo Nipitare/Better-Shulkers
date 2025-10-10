80ac2287da9cadddefe4099186e50122bcd966ee
Nipitare <nipitareyt@gmail.com>
2025-10-10 21:33:35 +0300

    Preview keybind
    Make the preview show up when pressing an keybind, also optimised some stuff (hopefully)

--- src/ShulkerRenderer.cpp
+++ src/ShulkerRenderer.cpp
@@ -38,8 +38,7 @@ void ShulkerRenderer::Render(MinecraftUIRenderContext* ctx, HoverRenderer* hover
 	// Only load inventory resources once
 	if (!hasLoadedTexture) {
 
-        mWhiteBackgroundTexture = ctx->getTexture("textures/ui/whiteBorder", true);
-        mBackgroundTexture = ctx->getTexture("textures/ui/purpleBorder", true);
+        mBackgroundTexture = ctx->getTexture("textures/ui/whiteBorder", true);
         mItemSlotTexture = ctx->getTexture("textures/gui/Whitegui", true);
 
         hasLoadedTexture = true;
@@ -48,18 +47,8 @@ void ShulkerRenderer::Render(MinecraftUIRenderContext* ctx, HoverRenderer* hover
     std::string hovertext = hoverRenderer->mFilteredContent.substr(0, hoverRenderer->mFilteredContent.find("Minecraft") + 9);
 
     const std::map<std::string, mce::Color> tintColor = {{"0", {0.65, 0.45, 0.64, 0} }, {"1", {1, 1, 1, 0}}, {"2", {0.61, 0.6, 0.56, 0}}, {"3", {0.27, 0.28, 0.30, 0}}, {"4", {0.13, 0.13, 0.15, 0}}, {"5", {0.50, 0.31, 0.17, 0}}, {"6", {0.67, 0.16, 0.15, 0}}, {"7", {1, 0.49, 0.066, 0}}, {"8", {1, 0.85, 0.15, 0}}, {"9", {0.49, 0.80, 0.1, 0}}, {"a", {0.36, 0.47, 0.12, 0}}, {"b", {0.09, 0.58, 0.62, 0}}, {"c", {0.25, 0.77, 0.94, 0}}, {"d", {0.22, 0.23, 0.66, 0}}, {"e", {0.51, 0.16, 0.72, 0}}, {"f", {0.81, 0.27, 0.75, 0}}, {"g", {1, 0.59, 0.73, 0}}};
-    // Find other color tints too other than red
-    const std::map<std::string, mce::Color> test = { {"0", {0.73, 0.06, 0.07, 0}} };
 
-    float textHeight = (countNewlines(hovertext) + 1) * 10.0f;
-    float textWidth = 0.0f;
-
-    try {
-        textWidth = ctx->getLineLength(*client.mMinecraftGame->mFontHandle.mDefaultFont, hovertext.substr(hovertext.find("minecraft:"), hovertext.find("_box") - hovertext.find("minecraft:")), 1.0f, false) * 1.3f;
-    }
-    catch (...) {
-        textWidth = ctx->getLineLength(*client.mMinecraftGame->mFontHandle.mDefaultFont, hovertext.substr(0, hovertext.find(std::format("{}r", "\xc2\xa7"))), 1.0f, false) * 1.1f;
-    }
+    float textHeight = (countNewlines(hoverRenderer->mFilteredContent) + 2) * 10.0f;
 
     float panelWidth = slotSize * 9;
     float panelHeight = slotSize * 3 + textHeight;
@@ -68,20 +57,16 @@ void ShulkerRenderer::Render(MinecraftUIRenderContext* ctx, HoverRenderer* hover
     float panelY = hoverRenderer->mCursorPosition.y + hoverRenderer->mOffset.y;
 
     // Draw the background panel
-    RectangleArea Shulkerbackground = {panelX - 4, panelX + panelWidth + 4, panelY + 4 + textHeight, panelY + panelHeight + 12};
-    RectangleArea Textbackground = {panelX - 4, panelX + textWidth + 4, panelY - 4, panelY + textHeight + 4};
+    RectangleArea Shulkerbackground = {panelX, panelX + panelWidth + 8, panelY + textHeight - 2, panelY + panelHeight + 6};
 
-    backgroundNineslice.Draw(Shulkerbackground, &mWhiteBackgroundTexture, ctx);
+    backgroundNineslice.Draw(Shulkerbackground, &mBackgroundTexture, ctx);
     ctx->flushImages(tintColor.at(boxcolor), 1.0f, flushString);
-
-    backgroundNineslice.Draw(Textbackground, &mBackgroundTexture, ctx);
-    ctx->flushImages(mce::Color::WHITE, 1.0f, flushString);
 
     // Draw the item slots
 	for (int x = 0; x < 9; x++) {
         for (int y = 0; y < 3; y++) {
             glm::tvec2<float> size(slotSize, slotSize);
-            glm::tvec2<float> position(panelX + slotSize * x, panelY + 8 + textHeight + slotSize * y);
+            glm::tvec2<float> position(panelX + 4 + slotSize * x, panelY + 2 + textHeight + slotSize * y);
 
             ctx->drawImage(mItemSlotTexture, &position, &size, &itemSlotUvPos, &itemSlotUvSize, 0);
         }
@@ -90,8 +75,6 @@ void ShulkerRenderer::Render(MinecraftUIRenderContext* ctx, HoverRenderer* hover
     // It's possible to tint the background here
     ctx->flushImages(tintColor.at(boxcolor), 1.0f, flushString);
 
-
-
     // Draw the item icons
     BaseActorRenderContext renderCtxPtr = BaseActorRenderContext(*ctx->mScreenContext, *ctx->mClient, *ctx->mClient->mMinecraftGame);
 
@@ -99,9 +82,9 @@ void ShulkerRenderer::Render(MinecraftUIRenderContext* ctx, HoverRenderer* hover
         for (int y = 0; y < 3; y++) {
             const ItemStack* itemStack = &shulkerInventory[index][y * 9 + x];
             if (itemStack->mItem == nullptr) continue;
-
-            float xPos = (x * slotSize) + borderSize + panelX;
-            float yPos = (y * slotSize) + borderSize + textHeight + panelY + 8;
+            
+            float xPos = (x * slotSize) + borderSize + panelX + 4;
+            float yPos = (y * slotSize) + borderSize + textHeight + panelY + 2;
 
             renderCtxPtr.itemRenderer->renderGuiItemNew(&renderCtxPtr, itemStack, 0, xPos - 1.f, yPos - 1.f, false, 1.f, 1.f, 1.125f);
         }
@@ -124,10 +107,10 @@ void ShulkerRenderer::Render(MinecraftUIRenderContext* ctx, HoverRenderer* hover
             if (itemStack->mItem == nullptr) continue;
             if (itemStack->mCount == 1) continue;
 
-            float top = (y * slotSize) + borderSize + textHeight + panelY + 8;
+            float top = (y * slotSize) + borderSize + textHeight + panelY + 2;
             float bottom = top + 16.f;
 
-            float right = (x * slotSize) + borderSize + panelX;
+            float right = (x * slotSize) + borderSize + panelX + 4;
             float left = right + 6.f;
 
             std::string text = std::format("{}", itemStack->mCount);
@@ -138,11 +121,5 @@ void ShulkerRenderer::Render(MinecraftUIRenderContext* ctx, HoverRenderer* hover
             ctx->drawDebugText(&rect, &text, &mce::Color::WHITE, 1.0f, ui::Left, &textData, &caretData);
         }
     }
-
-    ctx->flushText(0.0f);
-
-    RectangleArea textArea = { panelX, panelX + panelWidth, panelY, panelY + textHeight };
-    RectangleArea rect = { panelX, panelX + panelWidth, panelY, panelY + panelHeight };
-    ctx->drawDebugText(&textArea, &hovertext, &mce::Color::WHITE, 1.0f, ui::Left, &textData, &caretData);
     ctx->flushText(0.0f);
 }
